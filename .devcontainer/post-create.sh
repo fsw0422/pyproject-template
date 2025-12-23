87674c26a1c1e02a533b1cc52f94fd03c241187a
Sun Phil Kwon <fsw0422@gmail.com>
2025-12-23 21:30:38 +0000

    Made devcontainers config more generic

--- .devcontainer/post-create.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a template project for Python using Dev Containers.
 
 Refer [Builder Dockerfile](./Dockerfile) to identify dependencies of this project.
+
+Please add `.devconatiner/setup-custom-dev-env.sh` for any additional custom environment setup.
